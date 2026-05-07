198af11dc342db7a83a0896fdde1c2abe0fd8bf6
satyamshukla-wq <satyamshukla8010@gmail.com>
2026-05-07 19:57:03 +0530

    github link add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -122,7 +122,7 @@ const Navbar = () => {
             ))}
             <div className="flex space-x-6 pt-2">
               <a
-                href="https://github.com/Pawan-Pandey1"
+                href="https://github.com/satyamshukla-wq"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-orange-500"
